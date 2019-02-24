33be5e129cd6c1f615cec6fce0650b4ddd6dfc9c
kornelkarcz <kornel.karcz@outlook.com>
2019-02-24 19:36:44 +0100

    Category view

--- src/main/java/pl/coderslab/controller/CategoryController.java
+++ src/main/java/pl/coderslab/controller/CategoryController.java
@@ -2,7 +2,9 @@ package pl.coderslab.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.model.Category;
 import pl.coderslab.service.CategoryService;
 
 @Controller
@@ -11,4 +13,10 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @RequestMapping("/add")
+    public String addCategory(Model model) {
+        model.addAttribute("categoty", new Category());
+        return "category/add";
+    }
 }
--- src/main/webapp/WEB-INF/views/category/add.jsp
+++ src/main/webapp/WEB-INF/views/category/add.jsp
@@ -1,10 +1,10 @@
-
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Adding a category</title>
 </head>
 <body>
+<h1>Add a category</h1>
 
 </body>
 </html>
