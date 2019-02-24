a7c5971b242084e388f9174c11bf32e27ce9c513
kornelkarcz <kornel.karcz@outlook.com>
2019-02-24 16:10:55 +0100

    Add Subscription class

---
+++ src/main/java/pl/coderslab/model/Subscription.java
@@ -0,0 +1,79 @@
+package pl.coderslab.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "subscriptions")
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotNull
+    @Digits(integer = 5, fraction = 2)
+    private BigDecimal sum;
+
+    @NotNull
+    private Integer quantity;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    public Subscription() {
+    }
+
+    public Subscription(BigDecimal sum, Integer quantity, LocalDateTime startDate, LocalDateTime endDate) {
+        this.sum = sum;
+        this.quantity = quantity;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getSum() {
+        return sum;
+    }
+
+    public void setSum(BigDecimal sum) {
+        this.sum = sum;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+}
