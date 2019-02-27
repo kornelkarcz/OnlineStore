b817f7611d3a1029830a47af82cca092eb81e278
kornelkarcz <kornel.karcz@outlook.com>
2019-02-27 16:37:10 +0100

    Add sum method to cart

--- src/main/java/pl/coderslab/model/Cart.java
+++ src/main/java/pl/coderslab/model/Cart.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +14,8 @@ import java.util.List;
 public class Cart {
 
     List<CartItem> cartItems = new ArrayList<>();
+    private BigDecimal sum = new BigDecimal(0.00);
+
 
     public void addToCart(CartItem cartItem) {
         cartItems.add(cartItem);
@@ -20,6 +23,18 @@ public class Cart {
 
     public List<CartItem> getCartItems() {
         return cartItems;
+    }
+
+    public BigDecimal calculateSum(List<CartItem> list) {
+
+        for (CartItem temp : cartItems) {
+
+            BigDecimal price = temp.getProduct().getPrice();
+            BigDecimal quantity = BigDecimal.valueOf(temp.getQuantity());
+
+            sum = price.multiply(quantity);
+        }
+        return sum;
     }
 
 }
