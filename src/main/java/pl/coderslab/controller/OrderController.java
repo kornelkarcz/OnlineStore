f9437a686493f2c3069186761c3dc0b385006ef8
kornelkarcz <kornel.karcz@outlook.com>
2019-02-28 16:40:45 +0100

    Delete comment out

--- src/main/java/pl/coderslab/controller/OrderController.java
+++ src/main/java/pl/coderslab/controller/OrderController.java
@@ -3,7 +3,6 @@ package pl.coderslab.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import pl.coderslab.model.*;
@@ -12,7 +11,6 @@ import pl.coderslab.service.ProductInOrdersService;
 import pl.coderslab.service.UserService;
 
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -73,9 +71,4 @@ public class OrderController {
 
         return "order/pay";
     }
-
-//    @PostMapping("/pay")
-//    public String payForOrder(@Valid Payment payment) {
-//        return "order/success";
-//    }
 }
